061e2f90a8bec05f86f836043a2576f08322eada
Alexander Khalyapov <khalyapov@phystech.edu>
2019-01-22 14:21:51 +0300

    fix

--- Internal_energy.py
+++ Internal_energy.py
@@ -15,7 +15,7 @@ from Cell import z_0, r_0, volume, theta
 from State_functions import eta, rho_e
 from working_progonka import X, PHI
 
-from Pressure import P_e_approximation
+#from Pressure import P_e_approximation
 
 
 Z = [0] * (N + 1)
@@ -24,10 +24,8 @@ for i in range(N + 1):
 
 
 # КИНЕТИЧЕСКАЯ ЭНЕРГИЯ
-def E_k(T, rho): # без константы
+def E_k(T, rho): # c константой
     subfunc2 = []
-    subfunc22 = []
-
     max_i = 0
 
     for i in range(1, N + 1):
@@ -37,7 +35,42 @@ def E_k(T, rho): # без константы
     subfunc1 = [0] * (max_i + 1)
     subx = [0] * (max_i + 1)
     for i in range(max_i + 1):
-        subx[i] = X[i]
+        subx[i] = Z[i]
+    nadx = []
+    for i in range(max_i + 1, N + 1):
+        nadx.append(Z[i])
+
+    for i in range(max_i + 1):
+        subfunc1[i] = 4 / 5 * (PHI[i]) ** (5 / 2)
+
+    for i in range(max_i + 1, N + 1):
+        subfunc2.append(2*Z[i]**5*integral_3_2(PHI[i]/Z[i]**2))
+
+
+
+
+    a1 = scipy.integrate.simps(subfunc1, subx)
+    a2 = scipy.integrate.simps(subfunc2, nadx)
+
+
+    return (a1 + a2)*3*2**(1/2)/(math.pi**2)*volume(rho)*theta(T)**(5/2)
+
+
+
+# ПОТЕНЦИАЛЬНАЯ ЭНЕРГИЯ
+def E_p(T, rho):
+    const = 2 * 2 ** (1 / 2) * volume(rho) * theta(T) ** (5 / 2) / math.pi ** 2
+    subfunc2 = []
+    max_i = 0
+
+    for i in range(1, N + 1):
+        if PHI[i]/X[i] >= 10 ** 6:
+            max_i = i
+
+    subfunc1 = [0] * (max_i + 1)
+    subx = [0] * (max_i + 1)
+    for i in range(max_i + 1):
+        subx[i] = Z[i]
     nadx = []
     for i in range(max_i + 1, N + 1):
         nadx.append(X[i])
@@ -51,75 +84,66 @@ def E_k(T, rho): # без константы
 
 
 
-    a1 = scipy.integrate.trapz(subfunc1, subx)
+    a1 = scipy.integrate.simps(subfunc1, subx)
     a2 = scipy.integrate.trapz(subfunc2, nadx)
-    #print("a1=", a1)
-    #print('a2=', a2)
-
-
-    return a1 + a2
-
-
-# ПОТЕНЦИАЛЬНАЯ ЭНЕРГИЯ
-def E_p(T, rho):
-    const = 2 * 2 ** (1 / 2) * volume(rho) * theta(T) ** (5 / 2) / math.pi ** 2
-    return const * (integral_3_2(-eta(T, rho)) - 3* E_k(T, rho))
-
-
+    return const * (integral_3_2(-eta(T, rho)) - 3*(a1+a2))
+print(E_k(T,rho))
+print(E_p(T, rho))
 # ВНУТРЕННЯЯ ЭНЕРГИЯ ЭЛЕКТРОНОВ
 def E_e(T, rho):
-    return (2*integral_3_2(-eta(T,rho)) - 3*E_k(T,rho))*2**(1/2)/math.pi**2*theta(T)**(5/2)*4/3*math.pi*r_0(rho)**3
-
-
+    return E_k(T,rho)+E_p(T,rho)
+print(E_e(T,rho))
 # ПОЛНАЯ ЭНЕРГИЯ
 def E(T, rho):
     return 2.626 * 10 ** 3 / Atom_weight * (E_e(T, rho) + 0.76874512 * z ** (7 / 3) + 3 / 2 * theta(T))
+print(E(T,rho))
 
 
-ENERGY_ISOHORE_RHO = [[], [], [], [], [],[]]
-RHO = [[], [], [], [], [],[]]
-k = -3
-while k < 3:
-    rho_is = 0.001
-    while rho_is < 100:
-        RHO[k + 3].append(rho_is)
-        ENERGY_ISOHORE_RHO[k + 3].append(E(10 ** k, rho_is))
-        rho_is += 0.1
-    k += 1
-for i in range(5):
-    plt.plot(RHO[i], ENERGY_ISOHORE_RHO[i])
-#plt.xscale('log')
-#plt.yscale('log')
-plt.xlabel("rho")
-plt.ylabel('E')
-plt.title('Energy isohore')
-plt.show()
 
-ENERGY_ISOTERM_T = [[], [], [], [], []]
-TT = [[], [], [], [], []]
-k = -3
-while k < 2:
-    T_is = 0.001
-    while T_is < 10:
-        TT[k + 3].append(T_is)
-        ENERGY_ISOTERM_T[k + 3].append(E(T_is, 10 ** k))
-        T_is += 0.1
-    k +=1
-for i in range(5):
-    plt.plot(TT[i], ENERGY_ISOTERM_T[i])
-#plt.xscale('log')
-#plt.yscale('log')
-plt.xlabel("T")
-plt.ylabel('E')
-plt.title('Energy isoterm')
-plt.show()
-
-print(E_e(0.0001,0.0001))
-#должны быть равны
-
-print("E0 = ", -0.76874512421364*z**(7/3))
-
-
-print("3P_e*volume = ", 3*P_e_approximation(T, rho)*volume(rho))
-
-print("2*E_k+E_p = ", 2*E_k(T,rho)+E_p(T,rho))
+#ENERGY_ISOHORE_RHO = [[], [], [], [], [],[]]
+#RHO = [[], [], [], [], [],[]]
+#k = -3
+#while k < 3:
+#    rho_is = 0.001
+#    while rho_is < 100:
+#        RHO[k + 3].append(rho_is)
+#        ENERGY_ISOHORE_RHO[k + 3].append(E(10 ** k, rho_is))
+#        rho_is += 0.1
+#    k += 1
+#for i in range(5):
+#    plt.plot(RHO[i], ENERGY_ISOHORE_RHO[i])
+##plt.xscale('log')
+##plt.yscale('log')
+#plt.xlabel("rho")
+#plt.ylabel('E')
+#plt.title('Energy isohore')
+#plt.show()
+#
+#ENERGY_ISOTERM_T = [[], [], [], [], []]
+#TT = [[], [], [], [], []]
+#k = -3
+#while k < 2:
+#    T_is = 0.001
+#    while T_is < 10:
+#        TT[k + 3].append(T_is)
+#        ENERGY_ISOTERM_T[k + 3].append(E(T_is, 10 ** k))
+#        T_is += 0.1
+#    k +=1
+#for i in range(5):
+#    plt.plot(TT[i], ENERGY_ISOTERM_T[i])
+##plt.xscale('log')
+##plt.yscale('log')
+#plt.xlabel("T")
+#plt.ylabel('E')
+#plt.title('Energy isoterm')
+#plt.show()
+#
+#print(E_e(0.0001,0.0001))
+##должны быть равны
+#
+#print("E0 = ", -0.76874512421364*z**(7/3))
+#
+#
+#print("3P_e*volume = ", 3*P_e_approximation(T, rho)*volume(rho))
+#
+#print("2*E_k+E_p = ", 2*E_k(T,rho)+E_p(T,rho))
