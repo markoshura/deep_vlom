86c3067a78fbb744138df0d5d8bafdee4cef0092
Alexander Khalyapov <khalyapov@phystech.edu>
2019-04-26 17:30:10 +0300

    fix

--- cold_curve_aluminium.py
+++ cold_curve_aluminium.py
@@ -3,6 +3,7 @@ from math import  pi, log10
 from Cell import theta, r_0   , eta_0
 
 from Internal_energy import Energy
+from Pressure import P
 
 m = 401
 n = 1
@@ -16,6 +17,12 @@ TABLE_E[1][0] = -9.000
 for j in range(2, m + 1):
     TABLE_E[0][j] = TABLE_E[0][1] + (j - 1) * 0.01
 
+TABLE_P = [[0 for i in range(m + 1)] for j in range(n + 1)]
+TABLE_P[0][1] = - 2.000
+TABLE_P[1][0] = -9.000
+for j in range(2, m + 1):
+    TABLE_P[0][j] = TABLE_P[0][1] + (j - 1) * 0.01
+
 
 
 
@@ -23,15 +30,23 @@ for j in range(1, m + 1):
     T_h = 10 ** TABLE_E[1][0] / z**(4 / 3)
     rho_h = 10 ** TABLE_E[0][j] * Na * 11.19 * 1.4818 * 10**(-25) / Atom_weight / z
     TABLE_E[1][j] = Energy(T_h, rho_h, 1, 1) * z**(7 / 3)
-    q = 3 * pi / 2 * 1 / 2 / theta(T_h)**(3 / 2) / r_0(rho_h, 1)**3
-    #print("q = ", q)
-    #print(- (3 / 2 * q) ** (2 / 3))
-    print(TABLE_E[1][j])
-    #print(eta_0(T_h, rho_h))
+    TABLE_P[1][j] = P(T_h, rho_h) * z ** (10 / 3)
 
-#f = open('text4.txt', 'w')
-#for i in range(len(TABLE_E)):
-#    for j in range(len(TABLE_E[i])):
-#        f.write(str(TABLE_E[i][j]))
-#        f.write(" ")
-#    f.write("\n")
+
+f = open('cold_curve_pressure_aluminium.txt', 'w')
+for i in range(len(TABLE_P)):
+    for j in range(len(TABLE_P[i])):
+        f.write(str(TABLE_P[i][j]))
+        f.write(" ")
+    f.write("\n")
+
+    f = open('cold_curve_energy_aluminium.txt', 'w')
+    for i in range(len(TABLE_E)):
+        for j in range(len(TABLE_E[i])):
+            f.write(str(TABLE_E[i][j]))
+            f.write(" ")
+        f.write("\n")
+    T_h = 10 ** TABLE_E[1][0] / z**(4 / 3)
+    rho_h = 10 ** TABLE_E[0][j] * Na * 11.19 * 1.4818 * 10**(-25) / Atom_weight / z
+    TABLE_E[1][j] = Energy(T_h, rho_h, 1, 1) * z**(7 / 3)
+
---
+++ my_tables_aluminium.py
@@ -0,0 +1,56 @@
+from Tabular_values import  Na
+
+
+from Internal_energy import Energy
+from Pressure import P
+m = 401
+n = 501
+
+z = 13
+Atom_weight = 26
+
+TABLE_E = [[0 for i in range(m + 1)] for j in range(n + 1)]
+TABLE_E[0][1] = - 2.000
+TABLE_E[1][0] = 2.000
+for j in range(2, m + 1):
+    TABLE_E[0][j] = TABLE_E[0][1] + (j - 1) * 0.01
+
+for i in range(2, n + 1):
+    TABLE_E[i][0] = TABLE_E[1][0] + (i - 1) * (-0.02)
+
+
+TABLE_P = [[0 for i in range(m + 1)] for j in range(n + 1)]
+TABLE_P[0][1] = - 2.000
+TABLE_P[1][0] = 2.000
+for j in range(2, m + 1):
+    TABLE_E[0][j] = TABLE_P[0][1] + (j - 1) * 0.01
+
+for i in range(2, n + 1):
+    TABLE_P[i][0] = TABLE_P[1][0] + (i - 1) * (-0.02)
+
+for i in range(167, n + 1):
+    for j in range(1, m + 1):
+        T_h = 10 ** TABLE_E[i][0] / z**(4 / 3)
+        rho_h = 10 ** TABLE_E[0][j] * Na * 11.19 * 1.4818 * 10**(-25) / Atom_weight / z
+        TABLE_E[i][j] = Energy(T_h, rho_h, 1, 1) * z**(7 / 3)
+        TABLE_P[i][j] = P(T_h, rho_h) * z** (10 / 3)
+        print(i, j)
+
+f = open('energy_ferrum.txt', 'w')
+for i in range(len(TABLE_E)):
+    for j in range(len(TABLE_E[i])):
+        f.write(str(TABLE_E[i][j]))
+        f.write(" ")
+    f.write("\n")
+
+f = open('pressure_ferrum.txt', 'w')
+for i in range(len(TABLE_P)):
+    for j in range(len(TABLE_P[i])):
+        f.write(str(TABLE_P[i][j]))
+        f.write(" ")
+    f.write("\n")
+
+
+
+
+
