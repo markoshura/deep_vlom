1cb6f7a5b6491431f80a7f18b188257efde3bcc8
Alexander Khalyapov <khalyapov@phystech.edu>
2019-05-10 22:35:18 +0300

    fix

--- hi_function.py
+++ hi_function.py
@@ -1,7 +1,7 @@
-import math
+from math import pi
 import matplotlib.pyplot as plt
 from Changing_parameters import N
-from Dirak_functions import integral_minus_1_2, igrek_sht
+from Dirak_functions import integral_minus_1_2, igrek_sht, integral_sht_1_2
 
 from Atom_parameters import Atom_weight,z
 from Cell import z_0, r_0, volume, theta
@@ -13,8 +13,9 @@ from working_progonka import progonka, X
 def hi(T, rho):
 
 
-    PHI = progonka(T, rho, 1, 1)
-    const = 4 * (2 * theta(T))**(1/2) / math.pi * r_0(rho, 1)**2
+    PHI = progonka(T, rho, 196, 79)
+
+    const = 2**(7/6) * 3**(2/3) * pi**(-5 / 3) * theta(T)**(1/2) * volume(rho, 1) ** (2/3)
 
 
 
@@ -38,31 +39,31 @@ def hi(T, rho):
         for i in range(N, 1, -1):
 
 
-            h[i] =(2 * i + 1) / N**2
+            h[i] =(- 2 * i + 1) / N**2
 
-            k1[i] = h[i] * const * (1 / 2 * integral_minus_1_2(PHI[i] / X[i]) * Y[i] + X[i] * igrek_sht(PHI[i] / X[i]))
+            k1[i] = h[i] * const * (integral_sht_1_2(PHI[i] / X[i]) * Y[i] + X[i] * igrek_sht(PHI[i] / X[i]))
 
             q1[i] = h[i] * Z[i]
 
-            k2[i] = h[i] * const * (1 / 2 * integral_minus_1_2(((PHI[i] + PHI[i-1]) / 2)/((X[i] + X[i-1]) / 2))*(Y[i] + q1[i] / 2)+((X[i] + X[i-1]) / 2)*igrek_sht((PHI[i] + PHI[i-1]) / 2)/((X[i] + X[i-1])/2))
+            k2[i] = h[i] * const * (integral_sht_1_2(((PHI[i] + PHI[i-1]) / 2)/((X[i] + X[i-1]) / 2))*(Y[i] + q1[i] / 2) + ((X[i] + X[i-1]) / 2)*igrek_sht((PHI[i] + PHI[i-1]) / 2)/((X[i] + X[i-1])/2))
 
-            q2[i] = h[i]*(Z[i] + k1[i] / 2)
+            q2[i] = h[i] * (Z[i] + k1[i] / 2)
 
-            k3[i] = h[i] * const * (1 / 2 * integral_minus_1_2(((PHI[i] + PHI[i-1]) / 2)/((X[i] + X[i-1]) / 2))*(Y[i] + q2[i] / 2)+((X[i] + X[i-1]) / 2) * igrek_sht((PHI[i] + PHI[i-1]) / 2) / ((X[i] + X[i-1]) / 2))
+            k3[i] = h[i] * const * (integral_sht_1_2(((PHI[i] + PHI[i-1]) / 2)/((X[i] + X[i-1]) / 2))*(Y[i] + q2[i] / 2) + ((X[i] + X[i-1]) / 2) * igrek_sht((PHI[i] + PHI[i-1]) / 2) / ((X[i] + X[i-1]) / 2))
 
             q3[i] = h[i] * (Z[i] + k2[i] / 2)
 
-            k4[i] = h[i] * const * (1 / 2 *integral_minus_1_2(PHI[i-1] / X[i-1])*(Y[i] + q3[i]) + X[i-1] * igrek_sht(PHI[i-1] / X[i-1]))
+            k4[i] = h[i] * const * (integral_sht_1_2(PHI[i-1] / X[i-1]) * (Y[i] + q3[i]) + X[i-1] * igrek_sht(PHI[i-1] / X[i-1]))
 
             q4[i] = h[i] * (Z[i] + k3[i])
 
-            Y[i-1] = Y[i] + (-q1[i] - 2 * q2[i] - 2 * q3[i] - q4[i]) / 6
+            Y[i-1] = Y[i] + (q1[i] + 2 * q2[i] + 2 * q3[i] + q4[i]) / 6
 
-            Z[i-1] = Z[i] + (-k1[i] - 2 * k2[i] - 2 * k3[i] - k4[i]) / 6
+            Z[i-1] = Z[i] + (k1[i] + 2 * k2[i] + 2 * k3[i] + k4[i]) / 6
 
         Z[0] = Z[1] + 1 / 3 * (Z[1] - Z[2])
         i = 1
-        h[i] = (2 * i + 1) / N ** 2
+        h[i] = (- 2 * i + 1) / N ** 2
 
         k1[i] = h[i] * const * (1 / 2 * integral_minus_1_2(PHI[i] / X[i]) * Y[i] + X[i] * igrek_sht(PHI[i] / X[i]))
 
@@ -82,18 +83,18 @@ def hi(T, rho):
 
         q4[i] = h[i] * (Z[i] + k3[i])
 
-        Y[i - 1] = Y[i] + (-q1[i] - 2 * q2[i] - 2 * q3[i] - q4[i]) / 6
+        Y[i - 1] = Y[i] + (q1[i] + 2 * q2[i] + 2 * q3[i] + q4[i]) / 6
 
         return Y
 
     def sigma_2(sigma_0, sigma_1):
         HI1 = hi_function(sigma_1)
         HI0 = hi_function(sigma_0)
-        print(sigma_1 - (sigma_1 - sigma_0) * HI1[0] / (HI1[0] - HI0[0]))
+        print("sigma = ", sigma_1 - (sigma_1 - sigma_0) * HI1[0] / (HI1[0] - HI0[0]))
         return sigma_1 - (sigma_1 - sigma_0) * HI1[0] / (HI1[0] - HI0[0])
 
 
-    sigma_2 = sigma_2(1, -1)
+    sigma_2 = sigma_2(100, -100)
 
 
 
@@ -102,4 +103,4 @@ def hi(T, rho):
 
 
 
-print(hi(0.001, 1))
+print(hi(1, 1))
